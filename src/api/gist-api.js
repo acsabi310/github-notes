43b81d9d113b04a5a4304bac0578add257466d47
csaba.adam <csaba.adam@shiwaforce.com>
2019-11-13 18:01:06 +0100

    Resolve caching issue

--- src/api/gist-api.js
+++ src/api/gist-api.js
@@ -4,18 +4,22 @@ const BASE_URL = 'https://api.github.com/gists'
 export default {
 
   getGists () {
-    return axiosInstance.get(BASE_URL)
+    return axiosInstance.get(`${BASE_URL}?_=${timeStamp()}`)
   },
 
   getGist (id) {
-    return axiosInstance.get(`${BASE_URL}/${id}`)
+    return axiosInstance.get(`${BASE_URL}/${id}?_=${timeStamp()}`)
   },
 
   createGist (data) {
-    return axiosInstance.post(BASE_URL, data)
+    return axiosInstance.post(`${BASE_URL}?_=${timeStamp()}`, data)
   },
 
   patchGist (gistId, requestData) {
-    return axiosInstance.patch(`${BASE_URL}/${gistId}`, requestData)
+    return axiosInstance.patch(`${BASE_URL}/${gistId}?_=${timeStamp()}`, requestData)
   }
+}
+
+function timeStamp () {
+  return new Date().getTime()
 }
